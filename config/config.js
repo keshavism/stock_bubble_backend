dad36f727447c013cb9286f2a7a002fb1ed3ef2d
bimlesharma <bimlesh.mdb@gmail.com>
2025-09-21 21:21:34 +0530

    config update

--- config/config.js
+++ config/config.js
@@ -7,11 +7,9 @@ const DB_DETAILS = {
   user: process.env.MYSQL_DB_USERNAME,
   password: process.env.MYSQL_DB_PASSWORD,
   database: process.env.MYSQL_DB_SCHEMA,
-  connectTimeout: 120000, // 120 seconds
-  timeout: 120000, // 120 seconds
+  connectTimeout: 10000, // 10 seconds
+  // timeout: 120000, // 120 seconds
 };
-const ACCESS_TOKEN =
-  "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkaGFuIiwicGFydG5lcklkIjoiIiwiZXhwIjoxNzQwNzQxOTU2LCJ0b2tlbkNvbnN1bWVyVHlwZSI6IlNFTEYiLCJ3ZWJob29rVXJsIjoiIiwiZGhhbkNsaWVudElkIjoiMTEwMDM3MzQ5MSJ9.BfoM1E_rpgOttHJgr-w6oXPVny-B9M9pdmO0BzyJYPf3y_alzMrYJQ0WuaqZ31PlYRg5r6__O5C5uhW1lfqqJg";
 
 module.exports = {
   PORT: PORT,
